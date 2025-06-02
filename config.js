030e9bb35e0de6271d5f4d89277970f51c665abb
rikkqa43 <ranusuta8@gmail.com>
2025-06-02 17:48:25 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/CpvDXGQL/2351.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY, IM ALIVE NOW",
+
+};
